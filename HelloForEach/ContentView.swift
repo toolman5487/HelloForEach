e0eb8477d0dfb4e0e0a5eab6f4a124efe5b0b8ce
Willy <willy548798@gmail.com>
2024-12-16 18:00:46 +0800

    SwiftUI ForEach v1

--- HelloForEach/ContentView.swift
+++ HelloForEach/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    let lang = ["美洲","亞洲","歐洲","非洲","大洋洲"]
+    @State private var selectedItem = "亞洲"
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            HStack {
+                ForEach(lang, id: \.self) { item in
+                    Button {
+                        print(item)
+                        selectedItem = item
+                    } label: {
+                        Text(item)
+                            .frame(width:(UIScreen.main.bounds.width ) / 10)
+                            .foregroundColor(Color.black)
+                            .font(.system(size: 13))
+                            .padding()
+                    }.overlay {
+                        RoundedRectangle(cornerRadius: 30)
+                            .stroke(Color.black, lineWidth: selectedItem == item ? 3:1)
+                    }.animation(.easeInOut, value: selectedItem)
+                    
+                }
+            }
+            .padding()
+            List{
+                ForEach(lang, id: \.self) { item in
+                    Text(item)
+                }
+            }.listStyle(.plain)
         }
-        .padding()
     }
 }
 
