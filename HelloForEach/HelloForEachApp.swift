ba6322cbb4e4a0f85916664e820e2021b2bb7367
Willy <willy548798@gmail.com>
2024-12-16 17:16:05 +0800

    Initial Commit

---
+++ HelloForEach/HelloForEachApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloForEachApp.swift
+//  HelloForEach
+//
+//  Created by Willy Hsu on 2024/12/16.
+//
+
+import SwiftUI
+
+@main
+struct HelloForEachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
